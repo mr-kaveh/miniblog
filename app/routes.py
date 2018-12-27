f08de114e4829bead0a18bac7f6b944e7d7fec49
mr-kaveh <mr.hdavoodi@gmail.com>
2018-12-27 19:44:43 +0330

    Adding mock object

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'name': 'Hossein'}
+    return '''
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hossein</title>
+    </head>
+    <body>
+        <h1> Hello, ''' + user['name'] + '''</h1>
+       </body>'''
